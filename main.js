252e28774fcc23925a8fd60e023d98e02eb1b232
Magdalena Derek <magdalena.derek@tlen.pl>
2024-06-13 17:04:30 +0200

    Event click in JS (notes and coding)

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ console.log(`Witaj, nazywam się ${firstName} i mam ${age} lat.`);
 const header = document.querySelector('h1')
 
 console.log(header)
+
+const articleHeader = document.querySelector('.article__header--js')
+console.log(articleHeader)
+const button = document.querySelector('.article__button--js')
+console.log(button)
+
+const clickFunction = (e) => {
+    console.log(e);
+    articleHeader.innerHTML = 'Niespodzianka!'
+}
+
+button.addEventListener('click', clickFunction)
